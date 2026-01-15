e6f1ab75e98faefc62652b3c74b0a646a712bd46
yeswanth varma kanumuri <yeshuRAJ.9593@gmail.com>
2026-01-15 20:12:14 +0000

    Refactor code structure for improved readability and maintainability

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ExpeditionPlanner from './ExpeditionPlanner';
+import ExpeditionPlanner from './ExpeditionPlanner.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
